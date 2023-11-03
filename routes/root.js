b82e047683833c148c84a2ff15e51af4615f8b7f
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2023-11-03 08:17:38 +0000

    -- added - page styling draft

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const app = express()
 const Router = express.Router()
 const path = require('path') 
 
 Router.get('^/$|index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+})
+
+Router.get('/login|login(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'login.html'))
 })
 
 
