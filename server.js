7a92ae8fcf18a75e4c114da3bab8a7d4d9195346
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2023-11-07 07:14:49 +0000

    -- connected MongoDB & Mongoose

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const mongoose = require('mongoose')
+const connectDB = require('./config/connectDB')
 const logEvent = require('./middleware/logEvent')
+
 
 // PORT //
 const PORT = process.env.PORT || 3500
+
+// DB Connect //
+connectDB()
 
 // Event Logging //
 app.use(logEvent)
@@ -22,6 +29,8 @@ app.use(express.static('public'))
 // Routes //
 app.use('/', require('./routes/root'))
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+mongoose.connection.once('open', () => {
+  app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
+  console.log('Connected To Database')
 })
